7a404ab8b5a17f70038eb37e21c202b24cf8a699
Game Distribution <info@gamedistribution.com>
2016-07-24 15:05:45 +0300

    GD Construct2 Plugin for HTML5 Games
    Get your stats and monetize your games

---
+++ plugins/googleads/edittime.js
@@ -0,0 +1,108 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Vooxe Google Ads",
+		"id":			"vooxe",
+		"version":		"1.0",
+		"description":	"Google ads for Vooxe",
+		"author":		"Vooxe",
+		"help url":		"http://www.gamedistribution.com/Api/",
+		"category":		"Monetisation",
+		"type":			"object",			// appears in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+AddCondition(0, 0, "Is showing banner ad", "Ads", "Is showing banner ad", "True if currently showing a banner ad.", "IsShowingBanner");
+AddCondition(2, cf_trigger, "On Initiliaze ads", "Ads", "On Initiliaze ads", "Triggered when Vooxe ads system is ready.", "onInit");
+AddCondition(3, cf_trigger, "On Error occurs", "Ads", "On Error occurs", "Triggered when an error occured.", "onError");
+AddCondition(4, cf_trigger, "On Resume game", "Ads", "On Resume game", "Triggered when an banner closed or not recieved.", "onResumeGame");
+AddCondition(5, cf_trigger, "On Pause game", "Ads", "On Pause game", "Triggered when banner ad is received and ready to show.", "onPauseGame");
+
+// Actions
+AddStringParam("Key Name", "Key for analytics");
+AddAction(0, af_none, "Show Banner", "Ads", "Show banner with key <i>{0}</i> value", "Show a banner ad with given key on the screen while the game is running.", "ShowBanner");
+
+AddAction(1, 0, "Play", "Ads", "Counts Play", "Counts how many times clicked play button", "PlayLog");
+
+AddStringParam("Key Name", "Key for analytics");
+AddAction(2, af_none, "Custom Log", "Ads", "Saves CustomLog with key <i>{0}</i> value", "Counts how many times custom action called", "CustomLog");
+
+AddAction(3, 0, "Init API", "Ads", "Initiliaze ads system", "Loads Vooxe Html5 GdApi that hides the running game and display it when ready.", "InitAds");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [	
+	new cr.Property(ept_section,	"GD Account",	"",	"Account settings. You can get GameId and UserId from http://www.gamedistribution.com, register first to GD"),
+	new cr.Property(ept_text,		"GameId",		"",	"Your Game Id, you can find it after upload a game to GameDistribution"),
+	new cr.Property(ept_text,		"UserId",		"",	"Your UserId")
+	];
+
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+};
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+};
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+};
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+
+	// Set the default property values from the property table
+	this.properties = {};
+
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+
+	// Plugin-specific variables
+	this.just_inserted = false;
+};
+
+IDEInstance.prototype.OnCreate = function()
+{
+};
+
+IDEInstance.prototype.OnInserted = function()
+{
+};
+
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+};
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+};
+
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+};
+
+// Called to draw self in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+};
+
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+};
---
+++ plugins/googleads/runtime.js
@@ -0,0 +1,203 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.vooxe = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	var pluginProto = cr.plugins_.vooxe.prototype;
+
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		window["vooxe"]={};
+	};
+
+	var instanceProto = pluginProto.Instance.prototype;
+	
+	var isSupported = false;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{		
+		if (!window["vooxe"])
+		{
+			cr.logexport("[Construct 2] Vooxe Googleads plugin is required to show googleads ads with Cordova; other platforms are not supported");
+			return;
+		}		
+		
+		isSupported = true;
+		
+		this.vooxe = window["vooxe"];
+
+		// Attach events
+		var self = this;
+		
+		this.vooxe["onInit"] = function (data)
+		{
+			cr.logexport(data.Msg);
+			self.isShowingBannerAd = false;
+			self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onInit, self);
+		};
+		
+		this.vooxe["onError"] = function (data)
+		{
+			cr.logexport("Vooxe Googleads Plugin onError: "+data);
+			self.isShowingBannerAd = true;
+			self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onError, self);
+		};
+		
+		this.vooxe["onResumeGame"] = function ()
+		{
+			cr.logexport("Vooxe Googleads Plugin: onResume");
+			self.isShowingBannerAd = false;
+			self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onResumeGame, self);
+		};
+		
+		this.vooxe["onPauseGame"] = function ()
+		{
+			cr.logexport("Vooxe Googleads Plugin: onPauseGame");
+			self.isShowingBannerAd = true;
+			self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onPauseGame, self);
+		};
+
+		// Init GdApi
+		this.vooxe["InitAds"] = function ()
+		{
+			var settings = {
+				gameId: self.properties[0],
+				userId: self.properties[1],
+				resumeGame: self.vooxe.onResumeGame,
+				pauseGame: self.vooxe.onPauseGame,
+				onInit: self.vooxe.onInit, 
+				onError: self.vooxe.onError
+			};
+			(function(i,s,o,g,r,a,m){
+				i['GameDistribution']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)};i[r].l=1*new Date();a=s.createElement(o);m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a, m);
+			})(window, document, 'script', '//html5.api.gamedistribution.com/libs/gd/api.js', 'gdApi');
+			gdApi(settings);
+		}
+
+	};
+	
+	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+
+	Cnds.prototype.IsShowingBanner = function()
+	{
+		return this.isShowingBannerAd;
+	};
+	
+	Cnds.prototype.onInit = function()
+	{
+		return true;
+	};
+	
+	Cnds.prototype.onError = function(data)
+	{
+		return true;
+	};
+	
+	Cnds.prototype.onResumeGame = function(data)
+	{
+		return true;
+	};
+	
+	Cnds.prototype.onPauseGame = function(data)
+	{
+		return true;
+	};
+
+	pluginProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+
+	Acts.prototype.ShowBanner = function (key)
+	{
+		if (!isSupported) return;
+		
+		if (typeof (gdApi) === "undefined")
+		{
+			cr.logexport("Vooxe Googleads Plugin is not initiliazed.");
+			return;
+		}
+
+		gdApi.showBanner("{_key:"+key+"}");
+		cr.logexport("ShowBanner Key: "+key);
+		
+		this.isShowingBannerAd = true;
+	};
+
+	Acts.prototype.PlayLog = function ()
+	{
+		if (!isSupported) return;
+		
+		if (typeof (gdApi) === "undefined")
+		{
+			cr.logexport("Vooxe Googleads Plugin is not initiliazed.");
+			return;
+		}
+
+		gdApi.play();		
+	};
+
+	Acts.prototype.CustomLog = function (key)
+	{
+		if (!isSupported) return;
+		
+		if (typeof (gdApi) === "undefined")
+		{
+			cr.logexport("Vooxe Googleads Plugin is not initiliazed.");
+			return;
+		}
+
+		gdApi.customLog(key)
+	};
+
+	Acts.prototype.InitAds = function ()
+	{
+		if (!isSupported) return;
+
+		this.vooxe["InitAds"]();
+	};
+	
+	pluginProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+
+	pluginProto.exps = new Exps();
+
+}());
